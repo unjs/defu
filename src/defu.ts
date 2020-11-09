32b8bfa58443e436346d62e46b0370f44ddd8049
pooya parsa <pyapar@gmail.com>
2020-11-09 10:16:12 +0100

    feat: pass namespace to custom merger

--- src/defu.ts
+++ src/defu.ts
@@ -5,9 +5,9 @@ function isObject (val: any) {
 }
 
 // Base function to apply defaults
-function _defu<T> (baseObj: T, defaults: any, merger?: Merger): T {
+function _defu<T> (baseObj: T, defaults: any, namespace: string = '.', merger?: Merger): T {
   if (!isObject(defaults)) {
-    return _defu(baseObj, {}, merger)
+    return _defu(baseObj, {}, namespace, merger)
   }
 
   const obj = Object.assign({}, defaults)
@@ -23,14 +23,14 @@ function _defu<T> (baseObj: T, defaults: any, merger?: Merger): T {
       continue
     }
 
-    if (merger && merger(obj, key, val)) {
+    if (merger && merger(obj, key, val, namespace)) {
       continue
     }
 
     if (Array.isArray(val) && Array.isArray(obj[key])) {
       obj[key] = obj[key].concat(val)
     } else if (isObject(val) && isObject(obj[key])) {
-      obj[key] = _defu(val, obj[key], merger)
+      obj[key] = _defu(val, obj[key], (namespace ? `${namespace}.` : '') + key.toString(), merger)
     } else {
       obj[key] = val
     }
@@ -41,14 +41,14 @@ function _defu<T> (baseObj: T, defaults: any, merger?: Merger): T {
 
 // Create defu wrapper with optional merger and multi arg support
 function extend (merger?: Merger): DefuFn {
-  return (...args) => args.reduce((p, c) => _defu(p, c, merger), {} as any)
+  return (...args) => args.reduce((p, c) => _defu(p, c, '', merger), {} as any)
 }
 
 // Basic version
 const defu = extend() as Defu
 
 // Custom version with function merge support
-defu.fn = extend((obj, key, currentValue) => {
+defu.fn = extend((obj, key, currentValue, _namespace) => {
   if (typeof obj[key] !== 'undefined' && typeof currentValue === 'function') {
     obj[key] = currentValue(obj[key])
     return true
@@ -56,7 +56,7 @@ defu.fn = extend((obj, key, currentValue) => {
 })
 
 // Custom version with function merge support only for defined arrays
-defu.arrayFn = extend((obj, key, currentValue) => {
+defu.arrayFn = extend((obj, key, currentValue, _namespace) => {
   if (Array.isArray(obj[key]) && typeof currentValue === 'function') {
     obj[key] = currentValue(obj[key])
     return true
